38e9ae7833b04bf39dac71647a23b642cc9b28dc
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-06-01 10:46:26 +0200

    Added List Component, Button Component and passed onClick props from App.js to Button.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
-function App() {
-  return <h2>reminder project setup</h2>;
+import Button from './Components/Button'
+const App = () =>{
+
+  const onClickClearBirthDays = () => console.log('Has Hecho click en el botón');
+
+  return (
+    
+  <main>
+    <section className= "container">
+      <h3>0 birthdays today</h3>
+      <List/>
+      <Button onClick = {onClickClearBirthDays}/>
+
+    </section>
+
+
+
+  </main>
+
+  
+
+  ) 
 }
+
+
+
 
 export default App;
---
+++ src/Components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = ({onClick}) => {
+
+
+    return (
+        <button onClick = {onClick}> Clear Birthdays </button>
+
+    )
+}
+
+export default Button
